602251507e2c5f3300f6be8259c69b44dd8da46c
Matt Shepardson <lupemls@yahoo.com>
2020-05-25 08:46:35 -0700

    Added code to generate profile, switching to using puppeteer instead of electron

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # profile-generator
-This program will run through a command line using node, and will prompt the user for a color, becoming the background color for the cards. A PDF file will then be generated which will contain info about a github profile.
+This program will run through a command line using node, and will prompt the user for a color, becoming the background color for the cards. A PDF file will then be generated which will contain info about a github profile, including name, location, company they work for, their avatar, and data about their repos.
+
+Installation:
+Clone the repo and run "npm i" to download all node dependencies. 
+
+Usage:
+Run "node index" to run the app and enter your github username and select a color. Once answered, the app will access github data from their api through axios and the pdf will begin generating. Open up the pdf once created and you have your generated portfolio!
--- index.js
+++ index.js
@@ -1,134 +1,158 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
-const convertFactory = require('electron-html-to');
 const axios = require('axios');
-const url = `https://api.github.com/users/${process.argv[2]}`;
+const puppeteer = require('puppeteer');
 
-const conversion = convertFactory({
-    converterPath: convertFactory.converters.PDF
-});
+const init = () => {
+  inquirer
+    .prompt([
+      {
+        message: 'What is your github username?',
+        name: 'username',
+      },
+      {
+        type: 'list',
+        message: 'What color do you like most?',
+        name: 'color',
+        choices: ['green', 'blue', 'pink', 'red', 'orange', 'yellow', 'purple', 'cornflowerblue'],
+      },
+    ])
+    .then((res) => {
+      const url = `https://api.github.com/users/${res.username}`;
+      axios.get(url).then((response) => {
+        const userData = {
+          name: response.data.name,
+          username: res.username,
+          avatar: response.data.avatar_url,
+          company: response.data.company,
+          blog: response.data.blog,
+          location: response.data.location,
+          repos: response.data.public_repos,
+          followers: response.data.followers,
+          following: response.data.following,
+          bio: response.data.bio,
+          color: res.color,
+        };
 
-
-inquirer
-    .prompt({
-        message: 'What is your favorite color?',
-        name: 'color'
-    }).then(function (res) {
-        console.log(res.color);
-        let userData = {};
-        axios.get(url).then((response) => {
-            console.log(response)
-            userData = {
-                login : response.data.login,
-                avatar : response.data.avatar_url,
-                company : response.data.company,
-                blog : response.data.blog,
-                location : response.data.location,
-                repos : response.data.public_repos,
-                followers : response.data.followers,
-                following : response.data.following,
-                bio : response.data.bio
-            };
-            // console.log(userData);
-        });
         axios.get(`${url}/starred`).then((response) => {
-            userData.stars = 0;
-            response.data.forEach(repo => {
-                userData.stars += repo.watchers_count;
-            });
-            console.log(userData.stars);
-        });
-//         conversion({ html: `<!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-//         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-//     <title>Document</title>
-//     <style>
-//         body {
-//             background: cornflowerblue;
-//             text-align: center;
-//             overflow-x: hidden;
-//         }
-//         .main {
-//             left: 5%;
-//             top: 2em;
-//             width: 90%;
-//         }
-//         section {
-//             background: gray;
-//         }
-//         .stats {
-//             margin: 2em 10%;
-//             width: 80%;
-//         }
-//     </style>
-// </head>
-// <body>
-//     <div class="card main">
-//         <img src="..." class="card-img-top" alt="...">
-//         <div class="card-body">
-//             <h5 class="card-title">Hi!<br>My name is ${userData.name}</h5>
-//             <p class="card-text">Currently at ${userData.company}</p>
-//             <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-//             <a>${userData.location}</a>
-//             <a>Github</a>
-//             <a>Blog</a>
-//         </div>
-//     </div>
-//     <section>
-//         <br><br>
-//         <p>${userData.bio}</p>
-//         <div class="row">
-//             <div class="col-sm-1"></div>
-//             <div class="col-sm-5">
-//                 <div class="card stats">
-//                     <div class="card-body">
-//                         Public Repositories
-//                         <p>Number</p>
-//                     </div>
-//                 </div>
-//             </div>
-//             <div class="col-sm-5">
-//                 <div class="card stats">
-//                     <div class="card-body">
-//                         Followers
-//                         <p>Number</p>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//         <div class="row">
-//             <div class="col-sm-1"></div>
-//             <div class="col-sm-5">
-//                 <div class="card stats">
-//                     <div class="card-body">
-//                         GitHub Stars
-//                         <p>Number</p>
-//                     </div>
-//                 </div>
-//             </div>
-//             <div class="col-sm-5">
-//                 <div class="card stats">
-//                     <div class="card-body">
-//                         Following
-//                         <p>Number</p>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     </section>
-// </body>
-// </html>`}, function (err, result) {
-//             if (err) {
-//                 return console.error(err);
-//             }
+          let stars = 0;
+          response.data.forEach((repo) => {
+            stars += repo.watchers_count;
+          });
+          userData.stars = stars;          
+          
+          generateHTML(userData);
 
-//             console.log(result.numberOfPages);
-//             console.log(result.logs);
-//             result.stream.pipe(fs.createWriteStream('./profile.pdf'));
-//             conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
-//         });
-//     })
+        });
+      });
+    });
+}
+init();
+
+async function generateHTML(userData) {
+    try {const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.setContent(`<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css'
+        integrity='sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh' crossorigin='anonymous'>
+    <title>Document</title>
+    <style>
+        body {
+            background: ${userData.color};
+            text-align: center;
+            overflow-x: hidden;
+        }
+        img{
+            border-radius: 50%;
+            height: 100px;
+            margin-left: auto;
+            margin-right: auto;
+            width: 100px;
+        }
+        .main {
+            left: 5%;
+            top: 2em;
+            width: 90%;
+        }
+        section {
+            background: lightgray;
+        }
+        .stats {
+            margin: 2em 10%;
+            width: 80%;
+        }
+    </style>
+</head>
+<body>
+    <div class='card main'>
+        <img src='${userData.avatar}' alt='Avatar'>
+        <div class='card-body'>
+            <h5 class='card-title'>Hi!<br>My name is ${userData.name}</h5>
+            <p class='card-text'>Currently at ${userData.company}</p>
+            <a href='http://maps.google.com/maps?q=${userData}'>${userData.location}</a>
+            <a href='https://github.com/${userData.username}'>Github</a>
+            <a href='${userData.blog}'>Blog</a>
+        </div>
+    </div>
+    <section>
+        <br><br>
+        <p>${userData.bio}</p>
+        <div class='row'>
+            <div class='col-sm-1'></div>
+            <div class='col-sm-5'>
+                <div class='card stats'>
+                    <div class='card-body'>
+                        Public Repositories
+                        <p>${userData.repos}</p>
+                    </div>
+                </div>
+            </div>
+            <div class='col-sm-5'>
+                <div class='card stats'>
+                    <div class='card-body'>
+                        Followers
+                        <p>${userData.followers}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class='row'>
+            <div class='col-sm-1'></div>
+            <div class='col-sm-5'>
+                <div class='card stats'>
+                    <div class='card-body'>
+                        GitHub Stars
+                        <p>${userData.stars}</p>
+                    </div>
+                </div>
+            </div>
+            <div class='col-sm-5'>
+                <div class='card stats'>
+                    <div class='card-body'>
+                        Following
+                        <p>${userData.following}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>`);
+    await page.emulateMedia('screen');
+    await page.pdf({
+        path: `profile.pdf`,
+        format: 'A4',
+        printBackground: true
+    })
+    console.log("PDF Created");
+    await browser.close();
+    process.exit();
+    } catch (err) {
+        console.log(err)
+    throw err;
+    }
+}
